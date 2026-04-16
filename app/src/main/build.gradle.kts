5e818118c09e4f1bd634e540941b5bd450d3f606
pxndxlex <alex251079@gmail.com>
2026-04-16 09:29:10 -0400

    Activando dependencia de Mapbox y configurando repositorio de descargas

--- app/src/main/build.gradle.kts
+++ app/src/main/build.gradle.kts
@@ -13,6 +13,9 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        
+        // Inyectamos el token público de Mapbox desde GitHub Actions
+        resValue("string", "mapbox_access_token", "\"${System.getenv("MAPBOX_ACCESS_TOKEN") ?: ""}\"")
     }
 
     buildFeatures {
@@ -39,15 +42,14 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     
     // Jetpack Compose
-    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.ui:ui:1.6.3")
+    implementation("androidx.compose.ui:ui-graphics:1.6.3")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.material:material-icons-extended:1.6.3")
     
-    // GPS para el Velocímetro
+    // GPS para el velocímetro
     implementation("com.google.android.gms:play-services-location:21.1.0")
 
-    // Mapbox está comentado temporalmente hasta que configuremos tu Token Secreto
+    // ¡MAPBOX ESTÁ ACTIVO AHORA!
     implementation("com.mapbox.maps:android:11.2.0")
 }
