7109378fe77303698d5600fb1337ddb18c31d3a1
pxndxlex <alex251079@gmail.com>
2026-04-16 09:33:03 -0400

    Activando dependencia de Mapbox y configurando repositorio de descargas

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,34 +28,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "CarLauncher"
-include(":app")pluginManagement {
-    repositories {
-        google()
-        maven { url = uri("https://repo1.maven.org/maven2/") }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        maven { url = uri("https://repo1.maven.org/maven2/") }
-        mavenCentral()
-        
-        // Repositorio oficial de Mapbox para descargar la librería
-        maven {
-            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-            credentials {
-                username = "mapbox"
-                // Usa el token secreto (sk...) que guardamos en GitHub Actions
-                password = System.getenv("MAPBOX_DOWNLOADS_TOKEN") ?: ""
-            }
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-        }
-    }
-}
-rootProject.name = "CarLauncher"
 include(":app")
